dd8d01aa52411050af10d3d5441bad26be16b96c
Fynn Freyer <fynn.freyer@googlemail.com>
2024-10-11 12:39:59 +0200

    fix: only export calculator

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,13 @@
 mod accord;
 mod utils;
 
-use accord::App;
-use accord::calculator::Calculator;
-use utils::get_fasta_seq;
-
 use pyo3::prelude::*;
 
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
+use accord::calculator::Calculator;
 
-#[pyfunction]
-fn build_consensus(ref_path: String, aln_path: String) -> PyResult<String> {
-    let ref_seq = get_fasta_seq(&ref_path);
-    let consensus = App::calculate_consensus(ref_seq, aln_path);
-    Ok(consensus.sequence_string())
-}
-
-/// A Python module implemented in Rust.
-#[pymodule(name="_internal")]
+/// The internals of `accord-rs` that are implemented in Rust.
+#[pymodule(name = "_internal")]
 fn py_accord(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(build_consensus, m)?)?;
     m.add_class::<Calculator>()?;
     Ok(())
 }
