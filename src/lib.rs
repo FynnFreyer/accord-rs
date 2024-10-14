f09d89161afd34c1383b64da185e3ab5213571d8
Fynn Freyer <fynn.freyer@googlemail.com>
2024-10-14 13:45:54 +0200

    refactor: use declarative style for module declaration

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,9 @@ use accord::calculator::Calculator;
 
 /// The internals of `accord-rs` that are implemented in Rust.
 #[pymodule(name = "_internal")]
-fn py_accord(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_class::<Calculator>()?;
-    Ok(())
+mod py_accord {
+    use super::*;
+
+    #[pymodule_export]
+    use Calculator;
 }
