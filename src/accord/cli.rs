679c4f3256c5efa17a8aa46877bd0b310d286b13
Fynn Freyer <fynn.freyer@googlemail.com>
2024-10-21 13:56:18 +0200

    docs(CLI): add descriptions to CLI arguments

--- src/accord/cli.rs
+++ src/accord/cli.rs
@@ -5,13 +5,17 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 pub struct Args {
+    /// Path to a reference sequence.
     pub ref_path: String,
 
+    /// Path to a sorted SAM/BAM file containing reads that were aligned against the reference.
     pub aln_path: String,
 
+    /// Optionally, an out path. Defaults to stdout.
     #[arg(short, long, default_value_t = String::from("-"))]
     pub out_path: String,
 
+    /// Alignment quality settings for consensus generation.
     #[arg(short, long, default_value_t = String::new())]
     pub aln_reqs: String,
 }
